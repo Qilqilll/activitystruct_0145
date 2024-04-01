e66f16d414c61e25a5e7842ccef59141bea88b91
Qilqilll <aqilf612@gmail.com>
2024-04-01 13:48:50 +0700

    memasukan tipe data tambahan

--- 01-04-2024(2)/01-04-2024(2).cpp
+++ 01-04-2024(2)/01-04-2024(2).cpp
@@ -5,24 +5,3 @@ struct DetailAlamat {
 	string desa;
 	string kota;
 };
-
-struct Mahasiswa {
-	string nim;
-	string nama;
-	DetailAlamat alamat;
-	int umur;
-};
-
-int main() {
-	Mahasiswa mhs;
-	cout << "Nomor Mahasiswa : ";
-	cin >> mhs.nim;
-	cout << "Nama Mahasiswa : ";
-	cin >> mhs.nama;
-
-	cout << "Alamat Mahasiswa : "<<endl;
-	cout << "\t Nama Desa : ";
-	cin >> mhs.alamat.desa;
-	cout << "\t Nama kota : ";
-	cin >> mhs.alamat.kota;
-}
