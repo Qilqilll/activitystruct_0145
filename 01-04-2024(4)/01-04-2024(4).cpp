fed45a2b4e5bb8abf93f2e7d59e9d72a05f33444
Qilqilll <aqilf612@gmail.com>
2024-04-01 15:32:15 +0700

    menampilkan tulisan dan menginput data berurutan (ke-...)

--- 01-04-2024(4)/01-04-2024(4).cpp
+++ 01-04-2024(4)/01-04-2024(4).cpp
@@ -16,4 +16,20 @@ struct Mahasiswa {
 int main() {
     Mahasiswa mhs[3];
     for (int i = 0; i < 3; i++) {
-      
+        cout << "Data ke-" << (i + 1) << ":" << endl;
+        cout << "Nomor Mahasiswa : ";
+        cin >> mhs[i].nim;
+        cout << "Nama Mahasiswa : ";
+        cin >> mhs[i].nama;
+
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        getline(cin, mhs[i].alamat.desa);
+        cout << "\t Nama Kota : ";
+        getline(cin, mhs[i].alamat.kota);
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+        cout << endl;
+    }
+   
